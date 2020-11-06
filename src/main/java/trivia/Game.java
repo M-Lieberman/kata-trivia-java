7d59e6ac4bda7c062d3f589d9498ec412ea1bae4
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-11-06 22:07:00 +0000

    refactor Questions initalisation. Inline move methodPLayer method

--- src/main/java/trivia/Game.java
+++ src/main/java/trivia/Game.java
@@ -1,86 +1,17 @@
 package trivia;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Game implements IGame {
-    private List<Player> players = new ArrayList<>();
-
-
+    private final List<Player> players = new ArrayList<>();
     private final Questions questions = new Questions();
+
     int currentPlayer = 0;
     boolean isGettingOutOfPenaltyBox;
 
     public Game() {
-
-
     }
-    // static/nested class
-    static class Questions {
-        private final List<String> popQuestions = new LinkedList<>();
-        private final List<String> scienceQuestions = new LinkedList<>();
-        private final List<String> sportsQuestions = new LinkedList<>();
-        private final List<String> rockQuestions = new LinkedList<>();
-
-        public Questions() {
-            for (int i = 0; i < 50; i++) {
-                popQuestions.add("Pop Question " + i);
-                scienceQuestions.add(("Science Question " + i));
-                sportsQuestions.add(("Sports Question " + i));
-                rockQuestions.add(createRockQuestion(i));
-            }
-        }
-
-        private void askQuestion(int index) {
-            System.out.println(extractNextQuestion(index));
-        }
-
-
-        private String extractNextQuestion(int index) {
-            return switch (currentCategory(index)) {
-                case POP -> popQuestions.remove(0);
-                case SCIENCE -> scienceQuestions.remove(0);
-                case SPORTS -> sportsQuestions.remove(0);
-                case ROCK -> rockQuestions.remove(0);
-                default -> throw new IllegalStateException("Unexpected value: " + currentCategory(index).getLabel());
-            };
-        }
-
-        private QuestionCategory currentCategory(int index) {
-            return switch (index % 4) {
-                case 0 -> QuestionCategory.POP;
-                case 1 -> QuestionCategory.SCIENCE;
-                case 2 -> QuestionCategory.SPORTS;
-                case 3 -> QuestionCategory.ROCK;
-                default -> throw new IllegalStateException("Unexpected value: " + index % 4);
-            };
-        }
-
-        public String createRockQuestion(int index) {
-            return "Rock Question " + index;
-        }
-
-        enum QuestionCategory {
-            POP("Pop"),
-            SCIENCE("Science"),
-            SPORTS("Sports"),
-            ROCK("Rock");
-
-            private final String label;
-
-            QuestionCategory(String label) {
-                this.label = label;
-            }
-
-            public String getLabel() {
-                return label;
-            }
-        }
-    }
-
-
-
 
 
     public void add(String playerName) {
@@ -99,7 +30,7 @@ public class Game implements IGame {
                 isGettingOutOfPenaltyBox = true;
 
                 System.out.println(currentPlayer().name() + " is getting out of the penalty box");
-                movePlayer(roll);
+                currentPlayer().move(roll);
 
                 System.out.println(currentPlayer().name() + "'s new location is " + currentPlayer().place());
                 System.out.println("The category is " + questions.currentCategory(currentPlayer().place()).getLabel());
@@ -110,16 +41,12 @@ public class Game implements IGame {
             }
 
         } else {
-            movePlayer(roll);
+            currentPlayer().move(roll);
             System.out.println(currentPlayer().name() + "'s new location is " + currentPlayer().place());
             System.out.println("The category is " + questions.currentCategory(currentPlayer().place()).getLabel());
             questions.askQuestion(currentPlayer().place());
         }
 
-    }
-
-    private void movePlayer(int roll) {
-        currentPlayer().move(roll);
     }
 
 
@@ -192,6 +119,6 @@ public class Game implements IGame {
  * Move Method ---> Player.move(roll) -- with invariants
  * switch [expression]: 1) one lines/case, 2) default 3) no extra code in that method
  * Alt-J
- * Any field you create let it be private final at the start. NOT creating gettters and setters. If you have to create them, create them individually
+ * Any field you create let it be private final at the start. NOT creating getters and setters. If you have to create them, create them individually
  * records Java 16+ - immutable structs
  */
